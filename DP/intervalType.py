e4355ca69274f6de055cb056c50cd46d6f94928d
AtharvaW29 <atharavawagh29101@gmail.com>
2026-05-22 20:14:14 -0700

    Added Interval types

--- DP/intervalType.py
+++ DP/intervalType.py
@@ -18,3 +18,26 @@ class IntervalType:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         res.append(newInterval) # edge case if intervals = [] and newInterval != []
         return res
+    
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = []
+        sorted_intervals = sorted(intervals, key=lambda x: x[0])
+        res.append(sorted_intervals[0])
+        for s, e in sorted_intervals[0:]:
+            last_e = res[-1][1]
+            if s <= last_e:
+                res[-1][1] = max(e, last_e)
+            else:
+                res.append([s, e])
+        return res
+    
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        count = 0
+        sorted_intervals = sorted(intervals, key=lambda x: x[1])
+        temp_e = -float('inf')
+        for s, e in sorted_intervals:
+            if s >= temp_e:
+                temp_e = max(e, temp_e)
+            else:
+                count += 1               
+        return count
