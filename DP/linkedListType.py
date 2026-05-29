5fe6b52db114c295044c6f55fd2e7ee695a0957e
AtharvaW29 <atharavawagh29101@gmail.com>
2026-05-28 20:36:25 -0700

    Added removeNth From End

--- DP/linkedListType.py
+++ DP/linkedListType.py
@@ -104,3 +104,16 @@ class LinkedListType:
                 if node.next:
                     heapq.heappush(arr, (node.next.val, i, node.next))
             return dummy.next
+        
+        def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+            dummy = ListNode(0)
+            dummy.next = head
+            slow = dummy
+            fast = dummy
+            for _ in range(n):
+                fast = fast.next
+            while fast.next is not None:
+                slow = slow.next
+                fast = fast.next
+            slow.next = slow.next.next
+            return dummy.next
